5abd98821e32e29e8b468b82df474dc6b3e382b3
yun <595571349@qq.com>
2020-05-07 09:45:38 +0800

    add files

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-09 10:35:20
+ * @LastEditTime: 2020-03-28 15:27:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /zhimutong_company_backstage/vue.config.js
+ */
+
+
+module.exports = {
+  productionSourceMap: false,
+  publicPath: "./", //资源路径为相对路径 可以本地打开index.html
+  outputDir: 'docs'
+};
